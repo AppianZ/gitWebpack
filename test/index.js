a5266ae71699f182ada21314af908d24445e308b
AppianZ <zhang4baichi@sina.com>
2016-02-04 19:17:44 +0800

    webpack over

--- test/index.js
+++ test/index.js
@@ -2,17 +2,19 @@
  * Created by JW on 2016/2/3.
  */
 
-/*require('style!css!./index.css');
+require('style!css!./index.css');
 var h2 = document.createElement('h2');
 h2.innerHTML = '第二次出征webpack的第一次编译';
 h2.setAttribute('class','red');
 document.body.appendChild(h2);
-console.log('第二次出征webpack的第一次编译!!!~~');*/
+console.log('第二次出征webpack的第一次编译!!!~~');
 
+/*
 const arr=["str1",'str2','str3'];
 const arr2=[...arr,'str4'];
 import './index.css'
 console.log(arr2);
+*/
 
 
 /*const arr=["str1",'str2','str3'];
