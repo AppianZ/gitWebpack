b8f4a819e0ba8a1eed92761e4cd00f410094e2d7
AppianZ <zhang4baichi@sina.com>
2016-02-04 19:24:45 +0800

    webpack over

--- test/index.js
+++ test/index.js
@@ -2,12 +2,14 @@
  * Created by JW on 2016/2/3.
  */
 
+/*
 require('style!css!./index.css');
 var h2 = document.createElement('h2');
 h2.innerHTML = '第二次出征webpack的第一次编译';
 h2.setAttribute('class','red');
 document.body.appendChild(h2);
 console.log('第二次出征webpack的第一次编译!!!~~');
+*/
 
 /*
 const arr=["str1",'str2','str3'];
@@ -17,8 +19,8 @@ console.log(arr2);
 */
 
 
-/*const arr=["str1",'str2','str3'];
-const arr2=[...arr,'str4']
+const arr=["str1",'str2','str3'];
+const arr2=[...arr,'str4'];
 import Vue from 'vue'
 new Vue({
     el:"body",
@@ -26,4 +28,4 @@ new Vue({
         test:"haha"
     }
 })
-console.log(arr2)*/
+console.log(arr2);
